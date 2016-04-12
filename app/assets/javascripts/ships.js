bdc524251f8a38b075dabd67a776d1230c57a3fd
firestorm451 <mzwick1684@hotmail.com>
2016-04-12 16:41:27 -0400

    Explorer mode finished

--- app/assets/javascripts/ships.js
+++ app/assets/javascripts/ships.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
     var text_max = 170;
-    $('#textarea_feedback').html(text_max + ' characters remaining');
-
+    $('#textarea_feedback').html('You have ' + text_max + ' characters remaining');
     $('#ship_body').keyup(function() {
         var text_length = $('#ship_body').val().length;
         var text_remaining = text_max - text_length;
-
-        $('#textarea_feedback').html(text_remaining + ' characters remaining');
+        if (text_remaining >= 0) {
+          $('#textarea_feedback').html('You have ' + text_remaining + ' characters remaining.');
+          $('#textarea_feedback').removeClass("reddening").addClass("blackening")
+        }else{
+          $('#textarea_feedback').removeClass("blackening").addClass("reddening")
+          $('#textarea_feedback').html('You need to remove ' + (-1 * text_remaining) + ' characters to post this.');
+        };
     });
 });
