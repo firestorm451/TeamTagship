f049d56ed082fb7ea3999f7d0698c50fa7b42fb0
firestorm451 <mzwick1684@hotmail.com>
2016-04-12 14:40:25 -0400

    Working character count model

--- app/assets/javascripts/ships.js
+++ app/assets/javascripts/ships.js
@@ -1,3 +1,11 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+    var text_max = 170;
+    $('#textarea_feedback').html(text_max + ' characters remaining');
+
+    $('#ship_body').keyup(function() {
+        var text_length = $('#ship_body').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#textarea_feedback').html(text_remaining + ' characters remaining');
+    });
+});
